fa6c3fe373d47a3bda84b8656699d260e148b2d5
SeokEui Hong <hongs7701@naver.com>
2023-07-17 23:40:33 +0900

    [part 2] <airbnb clone coding 3> props를 airbnb_clone 코딩 Card.js 컴포넌트에 적용

--- blog/src/components/Contact.js
+++ blog/src/components/Contact.js
@@ -1,18 +1,18 @@
 import React from "react"
 
 
-export default function Contact(props){
+export default function Contact({img, name, phone, email}){
     return(
         <div className='contact-card'>
-            <img src={props.img}/>
-            <h3>{props.name}</h3>
+            <img src={img}/>
+            <h3>{name}</h3>
             <div className='info-group'>
                 <img src="./image/phone_icon.png" alt="phone_icon" />
-                <p>{props.phone}</p>
+                <p>{phone}</p>
             </div>
             <div className='info-group'>
                 <img src="./image/mail_icon.png" alt="email_icon" />
-                <p>{props.email}</p>
+                <p>{email}</p>
             </div>
         </div>
     )
