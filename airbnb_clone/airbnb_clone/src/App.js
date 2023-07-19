7658a3236f8b4ecdab4f75929a22caa8677161fd
SeokEui Hong <hongs7701@naver.com>
2023-07-20 00:27:26 +0900

    [part 2] <airbnb clone coding 4> 연습 예제 mapping components를 Joke 컴포넌트에서 연습

--- airbnb_clone/airbnb_clone/src/App.js
+++ airbnb_clone/airbnb_clone/src/App.js
@@ -2,19 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-/*
-Challenge: Pass props to the Card component and display that data
-
-- img ("katie-zaferes.png")
-- rating ("5.0")
-- reviewCount (6)
-- country (Whatever you want)
-- title ("Life Lessons with Katie Zaferes")
-- price (136)
-
-*/
+import Joke from './components/Joke';
+import jokesData from "./jokesData";
 
 function App() {
+
+  // const jokeElements = jokesData.map(joke => {
+  //   return <Joke setup= {joke.setup} punchline= {joke.punchline}/>
+  // })
   return (
     <div className="App">
       <Navbar />
@@ -22,11 +17,16 @@ function App() {
       <Card 
         img="./image/image 12.png"
         rating="5.0"
-        reviewCount = "6"
+        reviewCount = {6}
         country = "USA"
         title = "Life Lessons with Katie Zaferes"
-        price = "136"
+        price = {136}
       />
+
+      <div>
+        <h1>Practice</h1>
+          <Joke setup="바보" punchline="바보 아니다" />
+      </div>
     </div>
   );
 }
--- airbnb_clone/airbnb_clone/src/components/Navbar.js
+++ airbnb_clone/airbnb_clone/src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import airbnb_logo from '../image/airbnb_logo.png';
+
 
 export default function Navbar(){
     return(
       <nav className="navbar">
-        <img src={airbnb_logo} className='nav--logo' />
+        <img src="./image/airbnb_logo.png" className='nav--logo' />
       </nav>
     )
 }
